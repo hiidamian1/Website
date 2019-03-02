8510002f5d3731e703751abe11e5bf27625248ad
damian hii <dhii@uci.edu>
2019-03-02 03:55:17 -0800

    successfully redirects using window.location.href... is there a better solution?

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
-
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const connectSpotify = require("./routes/connectSpotify");
-
 app.use("/connectSpotify", connectSpotify);
+
+// If process.env.SPOTIFY_REFRESH_TOKEN is undefined, run authorization script. else, just call api to get songs
 
 //handle production
 if (process.env.NODE_ENV == "production") {
