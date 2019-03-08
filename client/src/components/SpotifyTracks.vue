00796c1ced6be6aca5a7fbfc113bb30ebfba06fe
damian hii <dhii@uci.edu>
2019-03-08 14:40:13 -0800

    responsive personal page using media queries and flexbox

--- client/src/components/SpotifyTracks.vue
+++ client/src/components/SpotifyTracks.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="tracks">
-    <div v-bind:key="tracks.indexOf(trackObj)" v-for="trackObj in tracks">
+    <div class="root" v-bind:key="tracks.indexOf(trackObj)" v-for="trackObj in tracks">
       <SpotifyTrack v-bind:track="trackObj.track"/>
     </div>
   </div>
@@ -24,5 +24,23 @@ export default {
   .tracks {
     display: flex;
     flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .root {
+    width: 100%;
+  }
+
+  @media(min-width: 768px){
+    .root {
+      width: 50%;
+    }
+  }
+
+  @media(min-width: 992px){
+    .root {
+      width: 33.3%;
+    }
   }
 </style>
